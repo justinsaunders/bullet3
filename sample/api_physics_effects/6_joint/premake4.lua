24ff95b76b17d369fc0fee641d22af3b6ca94641
Erwin Coumans <erwin.coumans@gmail.com>
2011-05-01 20:52:56 -0700

    add premake4 support for remaining samples

---
+++ sample/api_physics_effects/6_joint/premake4.lua
@@ -0,0 +1,24 @@
+	project "6_joint"
+		
+	kind "WindowedApp"
+	targetdir "../../../bin"
+	includedirs {"../../../include"}
+		
+	links {
+		"physicseffects2_lowlevel",
+		"physicseffects2_baselevel",
+		"physicseffects2_util",
+		"opengl32"
+	}
+	
+	flags       {"WinMain"}
+	
+	files {
+		"main.cpp",
+		"physics_func.cpp",
+		"barrel.h",
+		"landscape.h",
+		"../common/ctrl_func.win32.cpp",
+		"../common/perf_func.win32.cpp",
+		"../common/render_func.win32.cpp"
+	}
