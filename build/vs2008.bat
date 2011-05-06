7cd9722aa893e0de700bc588bf1caffcae04c1b3
Erwin Coumans <erwin.coumans@gmail.com>
2011-05-06 01:23:37 -0700

    fix batch file, force .bat files to use DOS/Windows line endings

--- build/vs2008.bat
+++ build/vs2008.bat
@@ -4,8 +4,8 @@ premake4 clean
 
 rem premake4 --no-pelibs vs2008
 rem premake4 --no-pedemos vs2008
-premake4 --no-bulletlibs --no-pelibs vs2008
-rem premake4 vs2008
+rem premake4 --no-bulletlibs --no-pelibs vs2008
+premake4 vs2008
 
 rename build vs2008
 pause
