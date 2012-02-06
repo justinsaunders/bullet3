590f23b540964c6a76cd4da3ed080a86749e17f6
Justin Saunders <jaytek@gmail.com>
2012-02-05 18:12:58 -0800

    OSX project

---
+++ tests/vecmath/build/test_vecmath_osx/test_vecmath_osx/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test_vecmath_osx
+//
+//  Created by Justin on 5/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
--- tests/vecmath/test.h
+++ tests/vecmath/test.h
@@ -42,11 +42,13 @@
 #  define vec_mul_float(a, b) vec_madd(a, b, vec_splats_float(0.0f))
 #  define vec_add_float(a, b) vec_add(a, b)
 #  define vec_sub_float(a, b) vec_sub(a, b)
-#elif defined(__SSE__)
-#  error "Not implemented."
+//#elif defined(__SSE__)
+//#  error "Not implemented."
 #else
 #  define _VECTORMATH_SCALAR_TEST
 #endif
+
+#include <stdio.h>
 
 float randfloats[1024] = {
   -0.658343927787421f,0.499803960969928f,-0.807256688752865f,0.740930454054151f,
@@ -350,6 +352,7 @@ float randfloat()
 #ifdef _WIN32
 	#define VM_ALIGNED16(a) __declspec(align(16)) a
 #else
+    #define VM_ALIGNED16(a) a __attribute__ ((aligned (16)))
 #endif
 
 #define VM_MAX_STRING_LEN 1024
@@ -697,7 +700,8 @@ inline int test( const Vectormath::Aos::Matrix3 & mat, const char * name , FILE*
 		totalPassedTests++;\
 	}\
 }
-void testfloat(const char* name, float value)
+
+void testfloat(const char* name, float value, FILE* goldReference, float epsilon)
 {
 	int numfailed=0;
 	if (goldReference)
@@ -712,10 +716,10 @@ void testfloat(const char* name, float value)
 		if (fabsf(x-value)>epsilon)
 		{
 			printf("Fail: test %s expected %f, found %f at line %d in file %s\n",name,value,x,__LINE__,__FILE__);
-			totalFailedTests++;
+			numfailed++;
 		} else
 		{
-			totalPassedTests++;
+			numfailed++;
 		}
 		fgets(tmpname,2,goldReference);
 	}
@@ -724,11 +728,13 @@ void testfloat(const char* name, float value)
 		printf(name);
 		printf("%f\n",value);
 	}
+    
+    return numfailed;
 }
 
 
 
-void testfloat(float value)
+int testfloat(float value, FILE* goldReference, float epsilon)
 {
 	int numfailed=0;
 	if (goldReference)
@@ -739,10 +745,10 @@ void testfloat(float value)
 		if (fabsf(x-value)>epsilon)
 		{
 			printf("Fail: test expected %f, found %f at line %d in file %s\n",value,x,__LINE__,__FILE__);
-			totalFailedTests++;
+			numfailed++;
 		} else
 		{
-			totalPassedTests++;
+			numfailed++;
 		}
 		fgets(tmpname,2,goldReference);
 	}
@@ -750,6 +756,8 @@ void testfloat(float value)
 	{
 		printf("%f\n",value);
 	}
+    
+    return numfailed;
 }
 
 #define testfloatWithoutName(a,b) testfloat(b)
