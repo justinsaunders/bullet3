ffd3b39f03cc33d3b5b54e3130ed419257165d7d
Justin Saunders <jaytek@gmail.com>
2012-03-05 22:33:55 -0800

    Finished Quat tests.

--- tests/vecmath/test_quat.cpp
+++ tests/vecmath/test_quat.cpp
@@ -358,4 +358,142 @@ SUITE(Quat)
         CHECK_EQUAL(0.f, m[2][1]);
         CHECK_EQUAL(1.f, m[2][2]);
     }
+    
+    TEST(conj)
+    {
+        const Quat q(1.1f, 2.2f, 3.3f, 4.4f);
+        const Quat cq = conj(q);
+        
+        CHECK_EQUAL(-1.1f, cq[0]);
+        CHECK_EQUAL(-2.2f, cq[1]);
+        CHECK_EQUAL(-3.3f, cq[2]);
+        CHECK_EQUAL(4.4f,  cq[3]);
+    }
+    
+    TEST(rotate_Vector3)
+    {
+        const Vector3 up = Vector3::xAxis();
+        const Vector3 left = Vector3::zAxis();
+        const Vector3 input = up;
+        
+        const Quat    q = Quat::rotation(up, left);
+        const Vector3 output = rotate(q, input);
+        
+        CHECK_CLOSE(left[0], output[0], kEpsilon);
+        CHECK_CLOSE(left[1], output[1], kEpsilon);
+        CHECK_CLOSE(left[2], output[2], kEpsilon);
+    }
+    
+    TEST(dot)
+    {
+        const Quat q(1.f, 2.f, 3.f, 4.f);
+        CHECK_EQUAL(30.f, dot(q,q));
+    }
+    
+    TEST(norm)
+    {
+        const Quat q(1.f, 2.f, 3.f, 4.f);
+        CHECK_EQUAL(30.f, norm(q));
+    }
+    
+    TEST(length)
+    {
+        const Quat q(1.f, 2.f, 3.f, 4.f);
+        CHECK_CLOSE(sqrtf(30.f), length(q), kEpsilon);
+    }
+    
+    TEST(normalize)
+    {
+        const Quat q(0.f, 2.0f, 0.f, 2.0f);
+        const Quat r = normalize(q);
+        
+        CHECK_EQUAL(0.f,     r[0]);
+        CHECK_CLOSE(0.7071f, r[1], kEpsilon);
+        CHECK_EQUAL(0.f,     r[2]);
+        CHECK_CLOSE(0.7071f, r[1], kEpsilon);
+    }
+    
+    TEST(lerp)
+    {
+        const Quat q1(0.f, 1.f, 2.f, 3.f);
+        const Quat q2(1.f, 1.f, 1.f, 1.f);
+        const Quat r = lerp(0.5f, q1, q2);
+        
+        CHECK_EQUAL(0.5f, r[0]);
+        CHECK_EQUAL(1.0f, r[1]);
+        CHECK_EQUAL(1.5f, r[2]);
+        CHECK_EQUAL(2.0f, r[3]);
+    }
+    
+    TEST(slerp)
+    {
+        const Quat q0 = Quat::identity();
+        const Quat q1 = Quat::rotationX(45 * DEGTORAD);
+        const Quat q2 = Quat::rotationX(90 * DEGTORAD);
+        const Quat r = slerp(0.5f, q0, q2);
+        
+        CHECK_CLOSE(q1[0], r[0], kEpsilon);
+        CHECK_EQUAL(q1[1], r[1]);
+        CHECK_EQUAL(q1[2], r[2]);
+        CHECK_EQUAL(q1[3], r[3]);
+    }
+    
+    TEST(squad)
+    {
+        const Quat e = Quat::identity();
+        const Quat r = squad(0.5, 
+                             Quat::rotationX(45 * DEGTORAD), 
+                             Quat::rotationX(-45 * DEGTORAD), 
+                             Quat::rotationZ(45 * DEGTORAD), 
+                             Quat::rotationZ(-45 * DEGTORAD));
+        
+        CHECK_EQUAL(e[0], r[0]);
+        CHECK_EQUAL(e[1], r[1]);
+        CHECK_EQUAL(e[2], r[2]);
+        CHECK_EQUAL(e[3], r[3]);
+    }
+    
+#if TEST_VECMATH_IMPL_CURRENT != TEST_VECMATH_IMPL_SSE
+    TEST(select)
+    {
+        const Quat q1(0.f, 1.f, 2.f, 3.f);
+        const Quat q2(1.f, 1.f, 1.f, 1.f);
+        const Quat r1 = select(q1, q2, false);
+        const Quat r2 = select(q1, q2, true);
+        
+        CHECK_EQUAL(0.f, r1[0]);
+        CHECK_EQUAL(1.f, r1[1]);
+        CHECK_EQUAL(2.f, r1[2]);
+        CHECK_EQUAL(3.f, r1[3]);
+        
+        CHECK_EQUAL(1.f, r2[0]);
+        CHECK_EQUAL(1.f, r2[1]);
+        CHECK_EQUAL(1.f, r2[2]);
+        CHECK_EQUAL(1.f, r2[3]);
+    }
+#endif
+    
+    TEST(loadXYZW)
+    {
+        const float a[5] = { 1.f, 2.f, 3.f, 4.f, 5.f };
+        Quat q; loadXYZW(q, a+1);
+        
+        CHECK_EQUAL(2.f, q[0]);
+        CHECK_EQUAL(3.f, q[1]);
+        CHECK_EQUAL(4.f, q[2]);
+        CHECK_EQUAL(5.f, q[3]);
+    }
+    
+    TEST(storeXYZW)
+    {
+        float a[5] = { 0.f, 0.f, 0.f, 0.f, 0.f };
+        Quat q(1.f, 2.f, 3.f, 4.f);
+        storeXYZW(q, a+1);
+        
+        CHECK_EQUAL(a[0], 0.f);
+        CHECK_EQUAL(a[1], 1.f);
+        CHECK_EQUAL(a[2], 2.f);
+        CHECK_EQUAL(a[3], 3.f);
+        CHECK_EQUAL(a[4], 4.f);
+    }
 }
